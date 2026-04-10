050d102b1621572ff71c994107cbd5e5b057d0c5
FilipWein <FilipWein123@gmail.com>
2026-04-10 11:48:09 +0200

    add ability to set output path in dump passes

--- optimizer/include/optimizer/passes/debug/dump_available_copy.hpp
+++ optimizer/include/optimizer/passes/debug/dump_available_copy.hpp
@@ -1,6 +1,7 @@
 #ifndef OPTIMIZER_PASSES_DEBUG_DUMP_AVAIL_COPY_HPP_INCLUDED
 #define OPTIMIZER_PASSES_DEBUG_DUMP_AVAIL_COPY_HPP_INCLUDED
 
+#include <filesystem>
 #include <optimizer/programIR/program_ir.hpp>
 
 namespace optimizer::passes
@@ -9,6 +10,11 @@ class DumpAvailCopy
 {
   public:
     void run(program::ProgramIR_sptr sala_ir);
+
+    void set_output_path(std::filesystem::path output_path);
+
+  private:
+    std::filesystem::path output_path_{};
 };
 } // namespace optimizer::passes
 
--- optimizer/include/optimizer/passes/debug/dump_liveness.hpp
+++ optimizer/include/optimizer/passes/debug/dump_liveness.hpp
@@ -1,6 +1,8 @@
 
 #ifndef OPTIMIZER_PASSES_DEBUG_DUMP_LIVENESS_HPP_INCLUDED
 #define OPTIMIZER_PASSES_DEBUG_DUMP_LIVENESS_HPP_INCLUDED
+#include <filesystem>
+
 #include <optimizer/programIR/ir_types.hpp>
 
 namespace optimizer::passes
@@ -9,6 +11,11 @@ class DumpLiveness
 {
   public:
     void run(program::ProgramIR_sptr sala_ir);
+
+    void set_output_path(std::filesystem::path output_path);
+
+  private:
+    std::filesystem::path output_path_{};
 };
 } // namespace optimizer::passes
 #endif
