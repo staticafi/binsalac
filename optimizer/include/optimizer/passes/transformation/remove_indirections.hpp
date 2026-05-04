8a44f145ce9d9884bf6652bd5844a38a3f49c1bb
FilipWein <FilipWein123@gmail.com>
2026-05-04 20:55:03 +0200

    unify interface

--- optimizer/include/optimizer/passes/transformation/remove_indirections.hpp
+++ optimizer/include/optimizer/passes/transformation/remove_indirections.hpp
@@ -8,19 +8,7 @@ namespace optimizer::passes
 class RemoveIndirections
 {
   public:
-    RemoveIndirections();
-    ~RemoveIndirections();
-
-    RemoveIndirections(const RemoveIndirections&)            = delete;
-    RemoveIndirections(RemoveIndirections&&)                 = delete;
-    RemoveIndirections& operator=(const RemoveIndirections&) = delete;
-    RemoveIndirections& operator=(RemoveIndirections&&)      = delete;
-
-    program::ProgramIR_sptr run(program::ProgramIR_sptr sala_ir);
-
-  private:
-    class Impl;
-    std::unique_ptr<Impl> pImpl_;
+    void run(program::ProgramIR_sptr sala_ir);
 };
 } // namespace optimizer::passes
 #endif
